1bb04fd6ffb07559d01ae67eec2580d859209c86
saurav565674-eng <saurav565674@gmail.com>
2026-09-11 12:22:09 +0530

    version1.0.0

---
+++ day2.py
@@ -0,0 +1,91 @@
+# is_raining= False
+# if is_raining == True:
+#     print("raining outside")
+# else:
+#     print("not raining")
+
+# person = int(input("enter age"))
+# if person >= 18:
+#     print ("person is 18 ")
+# else :
+#     print("doesn't match age")    
+
+# age= 17
+# has_id=True
+# if age >=18:
+#     if has_id:
+#         print("entry allowed")
+#     else :
+#         print("declined")
+# else:
+#     print("unkown")
+
+# n = int(input("enter no"))
+# if n%2 ==0 :
+#     print("no is even")
+# else:
+#     print("no is odd")
+
+# age = int(input("enter age"))
+# price = int(input("enter amount"))
+# if age <12 :
+#     print("teckit price",price-price*10/100)
+# else :
+#     print("full price",price)
+
+# mark = int(input("enter mark"))
+# if mark >=90 and mark<=100 :
+#     print("o")
+# elif mark>=80  and mark<=90:
+#     print("a")
+# elif mark>=65  and mark<=80:
+#     print("b")
+# elif mark>=35 and mark<=65:
+#     print("c")
+# elif mark >=0 and mark<=35:
+#     print("f")
+# else :
+#     print("invaild")
+
+# n = int(input(" enter no"))
+# if n>0:
+#     print("positive")
+# elif n==0:
+#     print("neutral")
+# elif n<0:
+#     print("negative")
+# else :
+#     print("invalid")
+
+# a= int(input ("enter value"))
+# b= int(input ("enter value"))
+# c =int(input ("enter value"))
+# if a>b:
+#     print("greater no",a)
+# elif b>c:
+#     print("greater no",b)
+# else:
+#     print(" greater no ",c)
+
+# year = int (input ("enter year"))
+# if year%4==0 and year%100!=0:
+#     print("its leap year")
+# else :
+#     print("not leap year")
+a= int (input(" enter value"))
+b= int(input("enter value"))
+n=int(input("enter no"))
+match n:
+    case 1: 
+        print("addition",a+b)
+    case 2:
+        print("substration",a-b)
+    case 3:
+        print("multiple",a*b)
+    case 4:
+        if b==0:
+            print("division is zero")
+        else:
+            print("division",a/b)
+    case _:
+        print("invalid case")
--- demo1.py
+++ demo1.py
@@ -7,10 +7,11 @@ c = a%60
 print(f"{a} is {b} hours {c} minutes")
  
 a= input("enter no")
-print(a[-1])
+print(a[-1])           
 a= int(input("enter no"))
 print(f"{a%10} last digit")
 
 b= input("trecher /student =")
 a= int(input("enter age no"))
 print("Eligible ",b == "student" and a < 20)
+#saurav
