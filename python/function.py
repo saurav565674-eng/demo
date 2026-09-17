b39188564b0ffef3ae7f3e4a5fd2c89a049f08a1
saurav565674-eng <saurav565674@gmail.com>
2026-09-17 13:43:47 +0530

    function of python

---
+++ python/function.py
@@ -0,0 +1,56 @@
+# def greet():
+#     print("hello")
+# greet()
+
+# def greet (name)
+#     print("hello",name)
+#     greet("yourname")
+
+# def pi():
+#     return 3.14
+# pi()
+
+# def add(a,b):
+#     return a+b
+
+# def name(name="student"):
+#     print("hello",name)
+# name()
+
+# name("sam")
+
+# def add(p1,p2):
+#     print("addition",p1+p2)
+#     print("sub",p1-p2)
+#     print("multiple",p1*p2)
+#     print("dev",p1/p2)
+
+# def num(n):
+#     print(n)
+#     if n>0:
+#         num(n-1)
+# num(34)
+
+# def fact(n):
+#     if n>1:
+#         return n * fact(n-1)
+#     else:
+#         return 1
+  
+# print(fact(5))
+
+# def square(n):
+#     return n*n
+# square =lambda n:n*n
+# print(square(6))
+
+# def add(n):
+#     return n*n
+# add =lambda n:n+n
+# print(add(6))
+# def fun(n):
+#     if n==0:
+#         return 
+#     fun(n-1)
+#     print(n)
+# fun(3)
